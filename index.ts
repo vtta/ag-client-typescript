5d0845f94bff871b77f91bc326c6692e7b090083
James Perretta <jameslp@umich.edu>
2018-11-28 12:53:09 -0500

    Exported Project in index.ts

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ export { HttpClient } from './src/http_client';
 
 export { Course, Semester } from './src/course';
 export { User, UserRoles } from './src/user';
+
+export { Project } from './src/project';
