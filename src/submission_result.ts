1ef131b78d5f5910a749c831cd0b4dc54e4d171e
James Perretta <jameslp@umich.edu>
2019-08-09 16:31:53 -0400

    Implemented result output endpoint functions.

--- src/submission_result.ts
+++ src/submission_result.ts
@@ -2,7 +2,288 @@ import { AGTestCaseFeedbackConfig } from "./ag_test_case";
 import { AGTestCommandFeedbackConfig } from "./ag_test_command";
 import { AGTestSuiteFeedbackConfig } from "./ag_test_suite";
 import { ID } from "./base";
+import { HttpClient, ProgressEventListener } from "./http_client";
 import { MutationTestSuiteFeedbackConfig } from "./mutation_test_suite";
+
+export enum FeedbackCategory {
+    normal = 'normal',
+    past_limit_submission = 'past_limit_submission',
+    ultimate_submission = 'ultimate_submission',
+    staff_viewer = 'staff_viewer',
+    max = 'max',
+}
+
+export async function get_submission_result(
+    submission_pk: ID,
+    feedback_category: FeedbackCategory
+): Promise<SubmissionResultFeedback> {
+    let response = await HttpClient.get_instance().get<SubmissionResultFeedback>(
+        `/submissions/${submission_pk}/results/?feedback_category=${feedback_category}`);
+    return response.data;
+}
+
+export async function get_ag_test_cmd_result_output_size(
+    submission_pk: ID,
+    ag_test_cmd_result_pk: ID,
+    feedback_category: FeedbackCategory,
+): Promise<AGTestCommandResultOutputSize> {
+    let response = await HttpClient.get_instance().get<AGTestCommandResultOutputSize>(
+        `/submissions/${submission_pk}/ag_test_cmd_results/${ag_test_cmd_result_pk}/output_size/`
+        + `?feedback_category=${feedback_category}`
+    );
+    return response.data;
+}
+
+export interface AGTestCommandResultOutputSize {
+    stdout: number | null;
+    stderr: number | null;
+    stdout_diff: number | null;
+    stderr_diff: number | null;
+}
+
+export async function get_ag_test_cmd_result_stdout(
+    submission_pk: ID,
+    ag_test_cmd_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/ag_test_cmd_results/${ag_test_cmd_result_pk}/stdout/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_ag_test_cmd_result_stderr(
+    submission_pk: ID,
+    ag_test_cmd_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/ag_test_cmd_results/${ag_test_cmd_result_pk}/stderr/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_ag_test_cmd_result_stdout_diff(
+    submission_pk: ID,
+    ag_test_cmd_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string[]> {
+    let response = await HttpClient.get_instance().get<string[]>(
+        `/submissions/${submission_pk}/ag_test_cmd_results/${ag_test_cmd_result_pk}/stdout_diff/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_ag_test_cmd_result_stderr_diff(
+    submission_pk: ID,
+    ag_test_cmd_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string[]> {
+    let response = await HttpClient.get_instance().get<string[]>(
+        `/submissions/${submission_pk}/ag_test_cmd_results/${ag_test_cmd_result_pk}/stderr_diff/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_ag_test_suite_result_output_size(
+    submission_pk: ID,
+    ag_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory
+): Promise<AGTestSuiteResultOutputSize> {
+    let response = await HttpClient.get_instance().get<AGTestSuiteResultOutputSize>(
+        `/submissions/${submission_pk}/ag_test_suite_results/`
+        + `${ag_test_suite_result_pk}/output_size/`
+        + `?feedback_category=${feedback_category}`
+    );
+    return response.data;
+}
+
+export interface AGTestSuiteResultOutputSize {
+    setup_stdout_size: number | null;
+    setup_stderr_size: number | null;
+}
+
+export async function get_ag_test_suite_result_setup_stdout(
+    submission_pk: ID,
+    ag_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/ag_test_suite_results/${ag_test_suite_result_pk}/stdout/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_ag_test_suite_result_setup_stderr(
+    submission_pk: ID,
+    ag_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/ag_test_suite_results/${ag_test_suite_result_pk}/stderr/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_mutation_test_suite_result_output_size(
+    submission_pk: ID,
+    mutation_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory
+): Promise<MutationTestSuiteResultOutputSize> {
+    let response = await HttpClient.get_instance().get<MutationTestSuiteResultOutputSize>(
+        `/submissions/${submission_pk}/student_test_suite_results/`
+        + `${mutation_test_suite_result_pk}/output_size/`
+        + `?feedback_category=${feedback_category}`
+    );
+    return response.data;
+}
+
+export interface MutationTestSuiteResultOutputSize {
+    setup_stdout_size: number | null;
+    setup_stderr_size: number | null;
+    get_student_test_names_stdout_size: number | null;
+    get_student_test_names_stderr_size: number | null;
+    validity_check_stdout_size: number | null;
+    validity_check_stderr_size: number | null;
+    grade_buggy_impls_stdout_size: number | null;
+    grade_buggy_impls_stderr_size: number | null;
+}
+
+export async function get_mutation_test_suite_result_setup_stdout(
+    submission_pk: ID,
+    mutation_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/student_test_suite_results/`
+        + `${mutation_test_suite_result_pk}/?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_mutation_test_suite_result_setup_stderr(
+    submission_pk: ID,
+    mutation_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/student_test_suite_results/`
+        + `${mutation_test_suite_result_pk}/?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_mutation_test_suite_result_get_student_test_names_stdout(
+    submission_pk: ID,
+    mutation_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/student_test_suite_results/`
+        + `${mutation_test_suite_result_pk}/get_student_test_names_stdout/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_mutation_test_suite_result_get_student_test_names_stderr(
+    submission_pk: ID,
+    mutation_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/student_test_suite_results/`
+        + `${mutation_test_suite_result_pk}/get_student_test_names_stderr/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_mutation_test_suite_result_validity_check_stdout(
+    submission_pk: ID,
+    mutation_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/student_test_suite_results/`
+        + `${mutation_test_suite_result_pk}/validity_check_stdout/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_mutation_test_suite_result_validity_check_stderr(
+    submission_pk: ID,
+    mutation_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/student_test_suite_results/`
+        + `${mutation_test_suite_result_pk}/validity_check_stderr/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_mutation_test_suite_result_grade_buggy_impls_stdout(
+    submission_pk: ID,
+    mutation_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/student_test_suite_results/`
+        + `${mutation_test_suite_result_pk}/grade_buggy_impls_stdout/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
+
+export async function get_mutation_test_suite_result_grade_buggy_impls_stderr(
+    submission_pk: ID,
+    mutation_test_suite_result_pk: ID,
+    feedback_category: FeedbackCategory,
+    on_download_progress?: ProgressEventListener
+): Promise<string> {
+    let response = await HttpClient.get_instance().get<string>(
+        `/submissions/${submission_pk}/student_test_suite_results/`
+        + `${mutation_test_suite_result_pk}/grade_buggy_impls_stderr/`
+        + `?feedback_category=${feedback_category}`,
+        {on_download_progress: on_download_progress}
+    );
+    return response.data;
+}
 
 export interface SubmissionResultFeedback {
     pk: ID;
